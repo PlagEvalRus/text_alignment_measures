62c369a9c7a8c075da6f64a57841a0345db4785c
denin <dvzubarev@yandex.ru>
2018-01-23 14:12:20 +0300

    write results to csv

--- eval_results.sh
+++ eval_results.sh
@@ -16,35 +16,123 @@ run_eval_for_task(){
     cat "$task_dir"/pairs | \
         sed 's/.txt//g' | \
         sed 's/ /-/' | \
+        sed 's/\r//' | \
         sed 's/$/.xml/' | \
         xargs -I{} cp "$results_dir"/{} "$local_results_dir"/
 
-    echo "============$title MICRO============"
-    python ./text_alignment_measures.py -p "$task_dir" -d "$local_results_dir" --micro
+    # macro_out="$(python ./text_alignment_measures.py -p "$tasks_dir" -d "$results_dir")"
+    # echo "$macro_out"
+    # make_csv_row "$run_name" "total" "$macro_out" "$micro_out"
 
     echo "============$title MACRO============"
-    python ./text_alignment_measures.py -p "$task_dir" -d "$local_results_dir"
+    local macro_out="$(python ./text_alignment_measures.py -p "$task_dir" -d "$local_results_dir")"
+    echo "$macro_out"
+
+    echo "============$title MICRO============"
+    local micro_out="$(python ./text_alignment_measures.py -p "$task_dir" -d "$local_results_dir" --micro)"
+    echo "$micro_out"
+
+    make_csv_row "$run_name" "$title" "$macro_out" "$micro_out"
 
     rm -r "$local_results_dir"
+}
+
+# Plagdet Score 0.375781297067
+# Recall 0.2570745778
+# Precision 0.745795503916
+# Granularity 1.0243902439
+parse_granularity(){
+   local output="$1"
+   gran="$(echo "$output" | grep -Po '(?<=Granularity ).*')"
+   echo "$gran"
+}
+
+parse_plagdet(){
+    local output="$1"
+    plagdet="$(echo "$output" | grep -Po '(?<=Plagdet Score ).*')"
+    echo "$plagdet"
+}
+
+parse_recall(){
+    local output="$1"
+    recall="$(echo "$output" | grep -Po '(?<=Recall ).*')"
+    echo "$recall"
+}
+
+parse_precision(){
+    local output="$1"
+    precision="$(echo "$output" | grep -Po '(?<=Precision ).*')"
+    echo "$precision"
+}
+
+round_val(){
+    local val="$1"
+    echo $(LC_ALL=C /usr/bin/printf '%.*f\n' $round_precision $val)
+}
+
+make_csv_header(){
+    if [ ! -e "$output_file" ]; then
+        echo "run,type,granularity,macro_precision,macro_recall,macro_plagdet,micro_precision,micro_recall,micro_plagdet" >> "$output_file"
+    fi
+}
+
+make_csv_row(){
+    local run="$1"
+    local type="$2"
+    local macro_output="$3"
+    local micro_output="$4"
+
+    gran=$(parse_granularity "$macro_output")
+    gran=$(round_val $gran)
+
+    row="\"$run\",\"$type\",$gran,$(make_row_from_std_metrics "$macro_output")"
+    row="$row,$(make_row_from_std_metrics "$micro_output")"
+
+    echo "$row" >> "$output_file"
+}
+
+make_row_from_std_metrics(){
+    local output="$1"
+    local prec=$(round_val $(parse_precision "$output"))
+    local rec=$(round_val $(parse_recall "$output"))
+    local plagdet=$(round_val $(parse_plagdet "$output"))
+    echo "$prec,$rec,$plagdet"
 }
 
 # set -e
 # set -xv
 
-results_dir="$1"
-if [ -z "$results_dir" ]; then
-    results_dir="results"
-fi
+results_dir="results"
+tasks_dir="."
+round_precision=3
+output_file="result.csv"
+while [ $# -gt 0 ] ; do
+    case "$1" in
+        -r) results_dir="$2"                 ; shift 2 ;;
+        -t) tasks_dir="$2"                   ; shift 2 ;;
+        -p) round_precision="$2"             ; shift 2 ;;
+        -o) output_file="$2"                 ; shift 2 ;;
+        *)            shift 1 ;;
+    esac
+done
 
+run_name="$(basename $results_dir)"
 
-echo "============TOTAL MICRO============"
-python ./text_alignment_measures.py -p tasks -d "$results_dir" --micro
+make_csv_header
 
 echo "============TOTAL MACRO============"
-python ./text_alignment_measures.py -p tasks -d "$results_dir"
+macro_out="$(python ./text_alignment_measures.py -p "$tasks_dir" -d "$results_dir")"
+echo "$macro_out"
 
-run_eval_for_task "tasks/generated_copypast_meta" "Generated copypast"
-run_eval_for_task "tasks/generated_paraphrased_meta" "Generated paraphrase"
+echo "============TOTAL MICRO============"
+micro_out="$(python ./text_alignment_measures.py -p "$tasks_dir" -d "$results_dir" --micro)"
+echo "$micro_out"
 
-run_eval_for_task "tasks/manually-paraphrased" "Manually paraphrased"
-run_eval_for_task "tasks/manually_paraphrased2" "Manually paraphrased 2"
+make_csv_row "$run_name" "total" "$macro_out" "$micro_out"
+
+
+run_eval_for_task "$tasks_dir/01-generated-copypaste" "Generated copypast"
+run_eval_for_task "$tasks_dir/02-generated-paraphrased" "Generated paraphrase"
+
+run_eval_for_task "$tasks_dir/03-manually-paraphrased" "Manually paraphrased"
+run_eval_for_task "$tasks_dir/04-manually-paraphrased-light" "Manually paraphrased light"
